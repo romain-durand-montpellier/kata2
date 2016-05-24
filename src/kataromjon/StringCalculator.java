4bd3082256e2237477792ebda8f540c833d9781f
Romain D <romain.durand.montpellier@gmail.com>
2016-05-24 16:13:24 +0200

    CustomCarrier
    FunctionalityCommentedBecauseNotCompilable

---
+++ src/kataromjon/StringCalculator.java
@@ -0,0 +1,28 @@
+package kata2;
+
+public class StringCalculator {
+
+	// Return Sum of values with param as string separate with comma
+	public int add(String values) {
+		int somme = 0;
+
+		// if values is empty return 0
+		if (values != "") {
+			// else return sum of string parsed to integer
+			String[] splitValues = values.split(",");
+			for (String i : splitValues) {
+
+				// if we have a return carrier...
+				if (i.contains("\n")) {
+					String[] splitCarriers = i.split("\n");
+					for (String c : splitCarriers) {
+						somme += Integer.parseInt(c);
+					}
+				} else {
+					somme += Integer.parseInt(i);
+				}
+			}
+		}
+		return somme;
+	}
+}
---
+++ src/kataromjon/StringCalculatorTest.java
@@ -0,0 +1,77 @@
+package kata2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	
+	private static final String EMPTY_PARAM = "";
+	private static final int EMPTY_PARAM_RESULT = 0;
+	
+	private static final String ONE_PARAM = "1";
+	private static final int ONE_PARAM_RESULT = 1;
+	
+	private static final String TWO_PARAM = "1,2";
+	private static final int TWO_PARAM_RESULT = 3;	
+	
+	private static final String INFINITE_PARAM = "1,2,3,4";
+	private static final int INFINITE_PARAM_RESULT = 10;
+	
+	private static final String CARRIER_PARAM = "1\n2,3";
+	private static final int CARRIER_PARAM_RESULT = 6;
+	
+	
+	private StringCalculator sc ;
+	public StringCalculatorTest() {
+		// Given
+		 sc = new StringCalculator();
+	}
+	
+	@Test
+	public void StringCalculatorShouldReturnZeroWithEmptyStringParam() {
+		// WHEN
+		int somme = sc.add(EMPTY_PARAM);
+
+		// THEN
+		assertEquals(EMPTY_PARAM_RESULT, somme);
+	}
+
+	@Test
+	public void StringCalculatorShouldReturnTheParamIfOnlyOneParam() {
+		// WHEN
+		int somme = sc.add(ONE_PARAM);
+
+		// THEN
+		assertEquals(ONE_PARAM_RESULT, somme);
+	}
+
+	@Test
+	public void StringCalculatorShouldReturnSumWithStringParamAsTwoIntegerSeparateWithComma() {
+		// WHEN
+		int somme = sc.add(TWO_PARAM);
+
+		// THEN
+		assertEquals(TWO_PARAM_RESULT, somme);
+	}
+	
+	@Test
+	public void StringCalculatorShouldReturnSumWithStringParamAsInfiniteIntegerSeparateWithComma() {
+		// WHEN
+		int somme = sc.add(INFINITE_PARAM);
+
+		// THEN
+		assertEquals(INFINITE_PARAM_RESULT, somme);
+	}	
+	
+	@Test
+	public void StringCalculatorShouldReturnSumWithStringParamAsInfiniteIntegerSeparateWithCommaAndCarrier() {
+		// WHEN
+		int somme = sc.add(CARRIER_PARAM);
+
+		// THEN
+		assertEquals(CARRIER_PARAM_RESULT, somme);
+	}	
+
+}
