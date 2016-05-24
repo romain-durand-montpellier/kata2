1de4c6a60c388c1c15e237c584358c14e4cd9c88
Romain D <romain.durand.montpellier@gmail.com>
2016-05-24 17:02:31 +0200

    CustomDelimWorking
    NowWorking, caution: 2 packages !

--- src/kataromjon/StringCalculator.java
+++ src/kataromjon/StringCalculator.java
@@ -4,28 +4,23 @@ public class StringCalculator {
 
 	// Return Sum of values with param as string separate with comma
 	public int add(String values) {
-		int somme = 0;
-		
-		String customDelimiter = ",";
-		if (values.startsWith("//") && values.length() >=2){
-			customDelimiter = String.valueOf( values.charAt(2)); 
-			values = values.substring(3, values.length()-3);
-		}
+		int somme = 0;		
+
 		// if values is empty return 0
-		if (values != "") {
+		if (!values.equals("")) {
+			String customDelimiter = ",";
+			if (values.startsWith("//") && values.length() >= 2) {
+				customDelimiter = String.valueOf(values.charAt(2));
+				values = values.substring(4);
+			}
+			
+			// Support return carrier
+			values = values.replace("\n", customDelimiter);
+			
 			// else return sum of string parsed to integer
 			String[] splitValues = values.split(customDelimiter);
 			for (String i : splitValues) {
-
-				// if we have a return carrier...
-				if (i.contains("\n")) {
-					String[] splitCarriers = i.split("\n");
-					for (String c : splitCarriers) {
-						somme += Integer.parseInt(c);
-					}
-				} else {
 					somme += Integer.parseInt(i);
-				}
 			}
 		}
 		return somme;
