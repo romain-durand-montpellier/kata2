67880ca9f489f150966cca7da03564a4208cb270
jonas Bardin <isher36@gmail.com>
2016-05-24 13:53:40 +0200

    Refactor

--- src/kata2/StringCalculator.java
+++ src/kata2/StringCalculator.java
@@ -5,12 +5,21 @@ public class StringCalculator {
 	public int add(String values) {
 		int somme = 0;
 
-		if (values == "") {
-			somme = 0;
-		}
-		//Simply return the param since there's nothing to add
-		else {
-			somme = Integer.parseInt(values);
+		// if values is empty return 0
+		if (values != "") {
+
+			// if values have only one interger un values , simply return the param since there's nothing to add
+			if ( !values.contains(",") )
+			{
+				somme = Integer.parseInt(values);				
+			}
+			// if more than one...
+			else
+			{
+				//TODO:				
+			}
+
+			
 		}
 		return somme;
 
